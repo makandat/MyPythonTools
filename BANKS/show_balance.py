3c71987a51d83919bb72dce717e6706c89fe5a24
makandat <tadnakam@outlook.jp>
2018-12-03 17:20:27 +0900

    format 修正

--- BANKS/show_balance.py
+++ BANKS/show_balance.py
@@ -10,12 +10,12 @@ Common.esc_print('cyan', '銀行別残高一覧')
 rows = client.query("SELECT * FROM BANKS")
 print("id  日付    銀行コード  口座     残高")
 for row in rows :
-  print("{0:4d} {1}  {2}  {3}  {4:,:}".format(row[0], row[1], row[2], row[3], row[4], row[5]))
+  print("{0:4d} {1}  {2}  {3}  {,:}".format(row[0], row[1], row[2], row[3], row[4], row[5]))
 
 print()
 Common.esc_print('cyan', '日付ごとの残高一覧')
 rows = client.query("SELECT * FROM vw_balance")
 print("日付    残高")
 for row in rows :
-  print("{0}  {1,:}".format(row[0], row[1]))
+  print("{0}  {,:}".format(row[0], row[1]))
 
